da75472f96c5b3dd81dc2120d81374bcf1ab8ea0
dhruvsp_ <pathak.dhruv03@gmail.com>
2024-07-10 19:31:36 +0400

    push

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -9,7 +9,6 @@ import Checkbox from '@mui/material/Checkbox'
 import Link from '@mui/material/Link'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth, db } from '../firebase/firebaseConfig'
@@ -68,7 +67,6 @@ const SignIN = () => {
         }}
       >
         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
           Sign in
